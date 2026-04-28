3e8c93193bc7f7ea47b37984052c1b332259d64e
koteenok <koteenok@proton.me>
2026-04-29 01:09:49 +0300

    Update library.sh

--- library.sh
+++ library.sh
@@ -1,6 +1,20 @@
 sudo apt update
 sudo apt upgrade
 sudo apt install neofetch
+
 CUR_DIR=$(pwd) && mkdir -p ~/.config/neofetch && cd ~/.config/neofetch
 curl -L -o config.conf https://raw.githubusercontent.com/chick2d/neofetch-themes/refs/heads/main/normal/talljoe.conf
 cd "$CUR_DIR" 
+
+sudo apt update && sudo apt install zsh -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsMono.zip
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+sed -i 's/^ZSH_THEME=".*"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' ~/.zshrc
+
+curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsMono.zip
+sudo apt update && sudo apt install unzip -y
+mkdir -p ~/.local/share/fonts
+unzip -o JetBrainsMono.zip -d ~/.local/share/fonts
+fc-cache -f -v
+rm -rf ~/fonts_temp
