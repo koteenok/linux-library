eab371ba1923839e4f0fb360dc08b0223107116c
koteenok <koteenok@proton.me>
2026-04-29 02:01:44 +0300

    Update library.sh

--- library.sh
+++ library.sh
@@ -1,14 +1,17 @@
-sudo apt update
-sudo apt upgrade
-sudo apt install neofetch
+#!/bin/bash
 
-CUR_DIR=$(pwd) && mkdir -p ~/.config/neofetch && cd ~/.config/neofetch
-curl -L -o config.conf https://raw.githubusercontent.com/chick2d/neofetch-themes/refs/heads/main/normal/config.conf
-cd "$CUR_DIR" 
+# 1. Обновление и база
+sudo apt update && sudo apt upgrade -y
+sudo apt install neofetch git curl -y
 
-sudo apt update && sudo apt install zsh -y
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+curl -LO https://github.com/fastfetch-cli/fastfetch/releases/download/2.62.1/fastfetch-linux-aarch64.deb && sudo apt install ./fastfetch-linux-aarch64.deb -y && rm fastfetch-linux-aarch64.deb
+
+sudo apt install zsh -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
+
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 sed -i 's/^ZSH_THEME=".*"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' ~/.zshrc
 
-source ~/.zshrc
+sudo chsh -s $(which zsh) $(whoami)
+
+echo "Готово! Перезайди в терминал и запусти p10k configure"
