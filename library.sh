ed9da50b6a466e212636cbd6596adb61e256fd4c
koteenok <koteenok@proton.me>
2026-04-29 00:47:49 +0300

    Update library.sh

--- library.sh
+++ library.sh
@@ -1,7 +1,6 @@
 sudo apt update
 sudo apt upgrade
 sudo apt install neofetch
-sudo apt install kitty
 CUR_DIR=$(pwd) && mkdir -p ~/.config/neofetch && cd ~/.config/neofetch
 curl -L -o config.conf https://raw.githubusercontent.com/chick2d/neofetch-themes/refs/heads/main/normal/talljoe.conf
 cd "$CUR_DIR" 
