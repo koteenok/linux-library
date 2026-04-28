cc84d98d43dddad22a53c67b0c0bbb4044fa0215
koteenok <koteenok@proton.me>
2026-04-29 01:59:05 +0300

    Update library.sh

--- library.sh
+++ library.sh
@@ -8,7 +8,6 @@ cd "$CUR_DIR"
 
 sudo apt update && sudo apt install zsh -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsMono.zip
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 sed -i 's/^ZSH_THEME=".*"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' ~/.zshrc
 
