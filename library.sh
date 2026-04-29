8265165e50d41f0624adfe3bd2266fd3acdf17e7
koteenok <koteenok@proton.me>
2026-04-29 20:16:53 +0300

    Simplify update commands in library.sh

--- library.sh
+++ library.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-# 1. Обновление и база
-sudo apt update && sudo apt upgrade -y
-
+sudo apt update -y
+sudo apt upgrade -y
 curl -LO https://github.com/fastfetch-cli/fastfetch/releases/download/2.62.1/fastfetch-linux-aarch64.deb && sudo apt install ./fastfetch-linux-aarch64.deb -y && rm fastfetch-linux-aarch64.deb
-
 sudo apt install zsh -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
-
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 sed -i 's/^ZSH_THEME=".*"/ZSH_THEME="powerlevel10k\/powerlevel10k"/g' ~/.zshrc
-
 sudo chsh -s $(which zsh) $(whoami)
-
 echo "type this command:exit"
